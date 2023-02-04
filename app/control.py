7184c2ca39c9a15a5b0004ee40a62dc929148d5b
Matthias Bendewald <matthias.bendewald@gmx.de>
2023-02-04 19:15:30 +0100

    feature: implement clean exiting

--- app/control.py
+++ app/control.py
@@ -22,20 +22,32 @@ with open("config.json") as configuration:
     wechselrichter = qcells.qcellDevice(qcellJson["token"], qcellJson["sn"], qcellJson["feedInThreshold"], feedInDelta)
 
     while(True):
-        print(wechselrichter.getStatus())
-        for dev in devices:
-            devices[dev].updateStatus()
-            devices[dev].printStatus()
-            noManualUpdates = (datetime.datetime.now() - devices[dev].lastManualInput) > handsoffTime
-            if(noManualUpdates):
-                print("no manual updates, can do stuff!")
-                if(wechselrichter.isFeedinHigh()):
-                    print("HIGH input, enable ac")
-                    devices[dev].activate()
+        try:
+            print(wechselrichter.getStatus())
+            for dev in devices:
+                devices[dev].updateStatus()
+                devices[dev].printStatus()
+                noManualUpdates = (datetime.datetime.now() - devices[dev].lastManualInput) > handsoffTime
+                if(noManualUpdates):
+                    print("no manual updates, can do stuff!")
+                    if(wechselrichter.isFeedinHigh()):
+                        print("HIGH input, enable ac")
+                        devices[dev].activate()
+                    else:
+                        if (wechselrichter.isFeedinLow()):
+                            print("LOW input, deactivate ac")
+                            devices[dev].deactivate()
                 else:
-                    if (wechselrichter.isFeedinLow()):
-                        print("LOW input, deactivate ac")
-                        devices[dev].deactivate()
-            else:
-                print ("There were manual updates, need to keep hands off\n")
-        time.sleep(300)
+                    print ("There were manual updates, need to keep hands off")
+            waitUntil = datetime.datetime.now() + datetime.timedelta(minutes=5)
+            print ("Done for now, continue at ", waitUntil)
+            while(waitUntil > datetime.datetime.now()):
+                time.sleep(1)
+        except:
+            print("exiting...")
+            for dev in devices:
+                try:
+                    devices[dev].deactivate()
+                except:
+                    print("error while deacivating", dev)
+                exit(0)
